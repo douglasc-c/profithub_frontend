f9576500c3808c4d9f343e81681d77f6a6b3ae26
fabriciotamai <59937379+fabriciotamai@users.noreply.github.com>
2024-01-03 19:26:55 -0300

    git(feat):chiliz

---
+++ src/components/Asks.tsx
@@ -0,0 +1,7 @@
+export function Asks(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/components/Bids.tsx
@@ -0,0 +1,7 @@
+export function Bids(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
