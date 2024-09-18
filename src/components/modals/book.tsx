241a50c0011e4c9e3b5ed80cd1a0b923bf1705a6
Douglas Camargo <douglas.camargo.ltda@gmail.com>
2024-09-17 23:11:35 -0300

    Update book.tsx

--- src/components/modals/book.tsx
+++ src/components/modals/book.tsx
@@ -42,7 +42,7 @@ export function Book({ isOpen, onClose, children, data, text }: BookProps) {
 
   const onSelectBuy = () => setIsSelected('buy')
   const onSelectSell = () => setIsSelected('sell')
-  console.log(data)
+
   return (
     <div className="fixed inset-0 bg-black bg-opacity-80 flex justify-center items-center z-50">
       <div className="bg-stone-950 rounded-t-md rounded-b-md shadow-lg max-w-md w-full border border-stone-800 z-50">
